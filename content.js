b96bcd1e0959603b8a64f410be623a27d6b28005
kentlius <kent.delatinos@gmail.com>
2023-09-11 09:34:30 +0700

    add border

--- content.js
+++ content.js
@@ -1,6 +1,6 @@
 const contents = [
   {
-    title: "ini judul",
+    title: "ini judul 1",
     content:
       "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem beatae adipisci animi praesentium temporibus ut iusto aperiam voluptatem, molestiae velit nemo eveniet odio cumque tempora asperiores incidunt. Maiores, voluptatibus sed.",
   },
@@ -10,17 +10,17 @@ const contents = [
       "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem beatae adipisci animi praesentium temporibus ut iusto aperiam voluptatem, molestiae velit nemo eveniet odio cumque tempora asperiores incidunt. Maiores, voluptatibus sed.",
   },
   {
-    title: "ini judul 2",
+    title: "ini judul 3",
     content:
       "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem beatae adipisci animi praesentium temporibus ut iusto aperiam voluptatem, molestiae velit nemo eveniet odio cumque tempora asperiores incidunt. Maiores, voluptatibus sed.",
   },
   {
-    title: "ini judul 2",
+    title: "ini judul 4",
     content:
       "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem beatae adipisci animi praesentium temporibus ut iusto aperiam voluptatem, molestiae velit nemo eveniet odio cumque tempora asperiores incidunt. Maiores, voluptatibus sed.",
   },
   {
-    title: "ini judul 2",
+    title: "ini judul 5",
     content:
       "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem beatae adipisci animi praesentium temporibus ut iusto aperiam voluptatem, molestiae velit nemo eveniet odio cumque tempora asperiores incidunt. Maiores, voluptatibus sed.",
   },
